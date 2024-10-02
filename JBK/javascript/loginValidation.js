1c6198517fe38c6b595abc355bd66459e8b35e9d
Chaitanya Gawali <chaitanyagawalipatil@gmail.com>
2024-10-02 18:42:22 +0530

    Update loginValidation.js

--- JBK/javascript/loginValidation.js
+++ JBK/javascript/loginValidation.js
@@ -4,6 +4,12 @@ function check() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
 
+    if (username == "Chaitanya" && password == 987654321) {
+        window.location.href = "/JBK/html/loginDone.html"
+    } else {
+        alert("Login Unsuccessful.!");
+    }
+
     // Check if username or password is empty
     if (username === "" || password === "") {
         alert("Username and password cannot be empty.");
@@ -29,14 +35,4 @@ function check() {
         alert("Password must contain at least one number.");
         return false;
     }
-
-    if (username == "Chaitanya" && password == 987654321) {
-        window.open = "/JBK/html/loginDone.html"
-    } else {
-        alert("Login Unsuccessful.!");
-    }
-
-    // If all validations pass
-    alert("Login successful!");
-    return true;
 }
